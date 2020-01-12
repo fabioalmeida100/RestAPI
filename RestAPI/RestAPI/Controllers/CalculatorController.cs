f838aaaac2a3776752ea92a93b3a04ebe6958f61
Fabio Almeida <fabio_almeida100@hotmail.com>
2020-01-11 22:02:32 -0300

    Operation sum

---
+++ RestAPI/RestAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+
+
+        // GET api/values/5/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            decimal soma;
+
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                soma = Convert.ToDecimal(firstNumber) + Convert.ToDecimal(secondNumber);
+                return Ok(soma.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        private bool IsNumeric(string firstNumber)
+        {
+            double number;
+            bool isNumber = 
+                double.TryParse(
+                    firstNumber, System.Globalization.NumberStyles.Any, 
+                    System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+    }
+}
